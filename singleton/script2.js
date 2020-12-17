8bb16052ab5b2749b522898d0e07e2bb6e0cc895
Igor Agapov <aio350@yahoo.com>
2020-12-17 17:21:45 +0500

    update

--- singleton/script2.js
+++
@@ -1,9 +0,0 @@
-class Singleton {
-  constructor() {
-    if (typeof Singleton.instance === 'object') {
-      return Singleton.instance
-    }
-    Singleton.instance = this
-    return this
-  }
-}
